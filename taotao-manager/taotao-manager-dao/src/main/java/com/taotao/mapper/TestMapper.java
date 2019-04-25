46105825e110e4c8fbc23c80265750a3d8169ddd
unknown <2497149285@qq.com>
2019-04-25 22:59:48 +0800

    first commit

---
+++ taotao-manager/taotao-manager-dao/src/main/java/com/taotao/mapper/TestMapper.java
@@ -0,0 +1,9 @@
+package com.taotao.mapper;
+/**
+ * 测试查询时间
+ * @author yzl
+ *
+ */
+public interface TestMapper {
+	public String queryNow();
+}
---
+++ taotao-manager/taotao-manager-interface/src/main/java/com/taotao/service/TestService.java
@@ -0,0 +1,9 @@
+package com.taotao.service;
+/**
+ * 测试时间接口
+ * @author 10205
+ *
+ */
+public interface TestService {
+	public String queryNow();
+}
